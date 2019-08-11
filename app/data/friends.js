4b3e20cddbab4e707e8905f1df1c0fd81935eca1
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-08-11 14:16:33 -0400

    implemented the logic to find friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,31 @@
-var faker = require('faker');
+var faker = require("faker");
 
-
-var friends =[
-	{
-		name :faker.name.findName(),
-		photo:faker.internet.avatar(),
-		scores:Array.from({length: 10}, () => Math.ceil(Math.random() * 5))
-      },
-      {
-		name :faker.name.findName(),
-		photo:faker.internet.avatar(),
-		scores:Array.from({length: 10}, () => Math.ceil(Math.random() * 5))
-      },
-      {
-		name :faker.name.findName(),
-		photo:faker.internet.avatar(),
-		scores:Array.from({length: 10}, () => Math.ceil(Math.random() * 5))
-	  }
+var friends = [
+  {
+    name: faker.name.findName(),
+    photo: faker.internet.avatar(),
+    scores: Array.from({ length: 10 }, () => Math.ceil(Math.random() * 5)),
+  },
+  {
+    name: faker.name.findName(),
+    photo: faker.internet.avatar(),
+    scores: Array.from({ length: 10 }, () => Math.ceil(Math.random() * 5)),
+  },
+  {
+    name: faker.name.findName(),
+    photo: faker.internet.avatar(),
+    scores: Array.from({ length: 10 }, () => Math.ceil(Math.random() * 5)),
+  },
+  {
+    name: faker.name.findName(),
+    photo: faker.internet.avatar(),
+    scores: Array.from({ length: 10 }, () => Math.ceil(Math.random() * 5)),
+  },
+  {
+    name: faker.name.findName(),
+    photo: faker.internet.avatar(),
+    scores: Array.from({ length: 10 }, () => Math.ceil(Math.random() * 5)),
+  },
 ];
 
 module.exports = friends;
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,18 +1,24 @@
-
 var friendsData = require("../data/friends");
 
-
-
-
 module.exports = function(app) {
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
 
-
-    app.get("/api/friends", function(req, res) {
-      res.json(friendsData);
+  app.post("/api/friends", function(req, res) {
+    var close_match = [];
+    friendsData.forEach(friend => {
+      var diff = 0;
+      for (var i = 0; i <= 9; i++) {
+        diff += Math.abs(friend.scores[i] - req.body.scores[i]);
+      }
+      close_match.push([friend, diff]);
     });
-
-    app.post("/api/friends", function(req, res) {
-        console.log("Data posted.....")
-      });
-
-    }
+    close_match.sort(function(a, b) {
+      return a[1] - b[1];
+    });
+    console.log(close_match[0]);
+    //friendsData.push(req.body);
+    res.json(close_match[0]);
+  });
+};
