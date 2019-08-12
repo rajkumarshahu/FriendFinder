eb7db359b07f2b34dcd5ea4c35fa2d41978ccf62
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2019-08-12 19:58:04 -0400

    changed logic of close match to exclude current user with name

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -7,18 +7,26 @@ module.exports = function(app) {
 
   app.post("/api/friends", function(req, res) {
     var close_match = [];
+    var matched_names = [];
     friendsData.forEach(friend => {
       var diff = 0;
       for (var i = 0; i <= 9; i++) {
         diff += Math.abs(friend.scores[i] - req.body.scores[i]);
       }
-      close_match.push([friend, diff]);
+      matched_names.push(friend.name);
+      if(!matched_names.includes(req.body.name)) {
+        close_match.push([friend, diff]);
+      }
     });
     close_match.sort(function(a, b) {
       return a[1] - b[1];
     });
-    console.log(close_match[0]);
-    friendsData.push(req.body);
+    //console.log(close_match[0]);
+
+    if(!matched_names.includes(req.body.name)) {
+      friendsData.push(req.body);
+    }
+
     res.json(close_match[0]);
   });
 };
